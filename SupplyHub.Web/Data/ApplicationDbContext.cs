6b4d5dae74bdd2a205e94bec0a645befc8b90239
Saprass <cruz_y_raya_2@hotmail.com>
2026-03-20 12:08:08 +0100

    Agregar archivos de proyecto.

---
+++ SupplyHub.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupplyHub.Web.Models;
+
+namespace SupplyHub.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories => Set<Category>();
+}
---
+++ SupplyHub.Web/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupplyHub.Web.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+}
